ad1c0b1253b274a518132b5a849dfc1018e20ee0
FIzzor <jps29@students.calvin.edu>
2016-09-23 16:03:05 -0400

    This add the toast when the app opens

--- app/src/main/java/com/example/cak37/calvindatingwip/MainActivity.java
+++ app/src/main/java/com/example/cak37/calvindatingwip/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cak37.calvindatingwip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "My heart I offer to you, promptly and sincerely.", Toast.LENGTH_SHORT).show();
     }
 }
