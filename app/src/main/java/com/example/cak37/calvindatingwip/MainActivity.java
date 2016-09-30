60d1f0681af4a4c927092bedd36ba96c9d4964da
meliornox <meliornox@gmail.com>
2016-09-30 14:52:27 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/cak37/calvindatingwip/MainActivity.java
+++ app/src/main/java/com/example/cak37/calvindatingwip/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.cak37.calvindatingwip;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//This will eventually be the entire dating app.
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //Initialization
     protected void onCreate(Bundle savedInstanceState) {
+        //Show the app as it was last time
         super.onCreate(savedInstanceState);
+        //Display the app
         setContentView(R.layout.activity_main);
     }
 }
